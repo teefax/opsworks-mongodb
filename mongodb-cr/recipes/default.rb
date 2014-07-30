42367b80b8a8f74760d600a4f4fd1dbfa789d1b3
Eike Bernhardt <ebe@cleverreach.com>
2014-07-30 12:29:14 +0200

    test for auth and bind_ip

--- mongodb-cr/recipes/default.rb
+++ mongodb-cr/recipes/default.rb
@@ -1,2 +1,5 @@
 include_recipe "mongodb::10gen_repo"
 include_recipe "mongodb::default"
+
+node.default['mongodb']['config']['bind_ip'] = node[:opsworks][:instance][:private_ip]
+node.default['mongodb']['config']['auth'] = true
